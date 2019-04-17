f135c252b98a6c17c67360f7cca2bbc220eaab26
SoaAlex <l.soaresalex@gmail.com>
2019-04-17 15:33:31 +0200

    Ajout Dijkstra

--- std/outils.h
+++
@@ -1,9 +0,0 @@
-#ifndef OUTILS_H_INCLUDED
-#define OUTILS_H_INCLUDED
-
-///Converti un nombre décimal en binaire + compte le nombre de 1 dans le nombre binaire obtenu (=nbr d'arêtes)
-int convertisseur_binaire(int nbr_binaire[], int nbr, int taille);
-///Réalise un calcul de puissance de 2
-int puissance(int nbr);
-
-#endif // OUTILS_H_INCLUDED
